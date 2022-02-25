d563ab40286c7c4a5c57a920c836458c886a704c
konstantin632 <98721681+konstantin632@users.noreply.github.com>
2022-02-26 00:39:22 +0300

    Delete task_2.py

--- homework_5/task_2/task_2.py
+++
@@ -1,7 +0,0 @@
-#2. Создать текстовый файл (не программно), сохранить в нём несколько строк, выполнить подсчёт строк и слов в каждой строке.
-with open('task_2.txt','r') as file:
-    list_string=file.readlines()
-    for i in range(len(list_string)):
-        sub_string = list_string[i].split()
-        print(f'{i+1} string has {len(sub_string)} words')
-
